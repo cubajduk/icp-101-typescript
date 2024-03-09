20b0259f9ff9c3fa574d798bff7fddd18e65dd87
Kuba <cubajduk@gmail.com>
2024-03-09 21:26:23 +0100

    Message3

--- scripts/delete.cmd
+++ scripts/delete.cmd
@@ -1,1 +1,1 @@
-curl -X DELETE http://bkyz2-fmaaa-aaaaa-qaaaq-cai.localhost:8000/messages/d8326ec8-fe70-402e-8914-ca83f0f1055b
+curl -X DELETE http://bkyz2-fmaaa-aaaaa-qaaaq-cai.localhost:8000/messages/585cb63a-eadb-43ab-9e29-a62c3047a916
